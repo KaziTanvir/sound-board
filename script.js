cae8b82c81f1ab5c895d5008690a01ccd47b126b
KaziTanvir <kazitanvir.bangladesh@gmail.com>
2021-07-22 16:49:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const sounds= ['sage', 'sova', 'omen', 'cypher', 'raze', 'jett', 'viper']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime=0;
+    })
+}
